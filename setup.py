cc8be018017fbf012da54621edce624758b8fae6
Aleksandr Mikhailov <dev@avidclam.com>
2019-11-18 14:45:07 +0300

    v0.1.1+setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import amshared
+
+with open("README.rst", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name=amshared.__name__,
+    version=amshared.__version__,
+    author=amshared.__author__,
+    author_email=amshared.__author_email__,
+    description=amshared.__description__,
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    python_requires='>=3.7.5',
+    license='MIT',
+    url="https://github.com/avidclam/amshared",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
